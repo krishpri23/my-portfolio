382d8a9b3e3291e03b6ab99b60f6692e528abe84
Krishnapriya <17042821+krishpri23@users.noreply.github.com>
2024-02-02 16:25:25 -0800

    added skillset

---
+++ src/components/SkillComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function SkillComponent({ imageSrc, skill }) {
+  return (
+    <div className="flex flex-col gap-5 justify-center items-center">
+      <img
+        src={imageSrc}
+        alt={`${skill} logo`}
+        className="w-14 h-14 rounded-xl object-cover"
+      />
+      <h1 className="font-bold text-black text-xl border-2 border-slate-800 px-5 py-1 rounded-3xl  hover:bg-slate-300 hover:text-slate-800">
+        {skill}
+      </h1>
+    </div>
+  );
+}
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,34 +1,28 @@
 import React from "react";
+import SkillComponent from "./SkillComponent";
 
 export default function Skills() {
+  const skills = [
+    { name: "Typescript", src: "/typescript.png" },
+    { name: "React", src: "/react.svg" },
+    { name: "Firebase Auth", src: "/firebase.png" },
+    { name: "Tailwind CSS", src: "/tailwind.png" },
+    { name: "Jest Testing ", src: "/jest.png" },
+    { name: "React-Hook-Form", src: "/rhf.png" },
+    { name: "Vite", src: "/vite.jpeg" },
+    { name: "Zod", src: "/zod.png" },
+    {
+      name: "Redux Toolkit",
+      src: "/redux.jpeg",
+    },
+  ];
   return (
     <section className=" flex flex-col gap-10 mx-auto w-full  bg-slate-100  mt-10 hover:border-2 hover:border-black-500 rounded-lg px-5 py-3">
       <h1 className="font-bold text-4xl text-center"> Techstack</h1>
       <div className="flex justify-center items-center  gap-5 flex-wrap lg:px-20 lg:py-10 mt-5  ">
-        <div className="techstack">
-          <h1> HTML </h1>
-        </div>
-        <div className="techstack">
-          <h1> CSS </h1>
-        </div>
-        <div className="techstack">
-          <h1> Javascript </h1>
-        </div>
-        <div className="techstack">
-          <h1> Typescript </h1>
-        </div>
-        <div className="techstack">
-          <h1> ReactJS </h1>
-        </div>
-        <div className="techstack">
-          <h1> NextJS </h1>
-        </div>
-        <div className="techstack">
-          <h1> TailwindCSS </h1>
-        </div>
-        <div className="techstack">
-          <h1> Vite </h1>
-        </div>
+        {skills.map((skill, index) => (
+          <SkillComponent key={index} imageSrc={skill.src} skill={skill.name} />
+        ))}
       </div>
     </section>
   );
